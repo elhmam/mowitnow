7d78f7f29275af4d1658e83eae083de1f78669d6
unknown <elhmam@UC140112.france.cfpb.intra>
2014-12-11 10:15:16 +0100

    Mise à jour de la classe principale

--- mowitnow/mowitnow-core/src/main/java/fr/mowitnow/core/app/App.java
+++ mowitnow/mowitnow-core/src/main/java/fr/mowitnow/core/app/App.java
@@ -18,6 +18,7 @@ import fr.mowitnow.core.parser.MowerParserImpl;
 
 /**
  * Classe principale.
+ * 
  * @author elhmam
  */
 public class App {
@@ -25,13 +26,16 @@ public class App {
      * LOGGER.
      */
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     /**
      * Constructeur privé.
      */
     private App() {
     }
+
     /**
      * Méthode principale.
+     * 
      * @param args
      *            : arguments à passer au lancement du programme
      */
@@ -46,17 +50,17 @@ public class App {
 	try {
 	    lines = FileUtils.readLines(new File(args[0]));
 	} catch (IOException e1) {
-	    LOGGER.error(e1.getMessage(),e1);
+	    LOGGER.error(e1.getMessage(), e1);
 	}
 
-	MowerParser mowerParser = new MowerParserImpl();
-	List<Mower> mowers = new ArrayList<>();
-
 	if (!lines.isEmpty()) {
+	    MowerParser mowerParser = new MowerParserImpl();
+	    List<Mower> mowers = new ArrayList<>();
+
 	    try {
 		mowers = mowerParser.loadMowers(lines);
 	    } catch (MowerParserException e) {
-		LOGGER.error(e.getMessage(),e);
+		LOGGER.error(e.getMessage(), e);
 	    }
 
 	    MowerBehavior mowerBehavior = new MowerBehaviorImpl();
